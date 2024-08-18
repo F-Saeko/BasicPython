bd87b9de90370927f2d573f7fb47a29d7b3f82bd
F-Saeko <s.fuwasaki.24.8.1566@4nonome.com>
2024-08-18 22:59:17 +0900

    問1完了

--- prime_number.py
+++ prime_number.py
@@ -2,12 +2,26 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+a = int(a)
+b = int(b)
+
 if a <= 1:
-    print("素数ではありません")
+    print(str(a) + "は素数ではありません")
 else:
     for i in range(2, int(a**0.5)+1):
         if a % i == 0:
-            print("素数ではありません")
+            print(str(a) + "は素数ではありません")
             break
-        else:
-            print("素数です")
+    else:
+        print(str(a) + "は素数です")
+
+if b <= 1:
+    print(str(b) + "は素数ではありません")
+else:
+    for i in range(2, int(b**0.5)+1):
+        if b % i == 0:
+            print(str(b) + "は素数ではありません")
+            break
+    else:
+        print(str(b) + "は素数です")
+        
