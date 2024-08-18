5524acb18959bed560898b009e9d236d46ff186e
F-Saeko <s.fuwasaki.24.8.1566@4nonome.com>
2024-08-18 22:23:43 +0900

    commit1

--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,12 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+if a == 1:
+    print(素数ではありません)
+else:
+    for i=2 in range(int(num**(1/2)+1):
+        if a % i == 0:
+            print(素数ではありません)
+            break
+        else:
+            print(素数です)
