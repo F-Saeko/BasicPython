6451248565089e67492148bc3b1cc42e15dd3f95
F-Saeko <s.fuwasaki.24.8.1566@4nonome.com>
2024-08-19 16:42:00 +0900

    Python入門_問1の回答2

--- quadratic_formula.py
+++ quadratic_formula.py
@@ -1,3 +1,4 @@
+# (1)
 a = 1
 b = -6
 c = 9
@@ -17,19 +18,23 @@ x2 = ((-b-((b**2)-(4*a*c))**(1/2)))/(2*a)
 print(x1, x2)
 
 # (3)
+from fractions import Fraction
+
 a = 8
 b = -6
 c = -35
 
 x1 = ((-b+((b**2)-(4*a*c))**(1/2)))/(2*a)
 x2 = ((-b-((b**2)-(4*a*c))**(1/2)))/(2*a)
-print(x1, x2)
+print(Fraction(x1), Fraction(x2))
 
 #(4)
+import cmath
+
 a = 1
 b = 0
 c = 1
 
-x1 = ((-b+((b**2)-(4*a*c))**(1/2)))/(2*a)
-x2 = ((-b-((b**2)-(4*a*c))**(1/2)))/(2*a)
+x1 = ((-b+cmath.sqrt((b**2)-(4*a*c))))/(2*a)
+x2 = ((-b-cmath.sqrt((b**2)-(4*a*c))))/(2*a)
 print(x1, x2)
