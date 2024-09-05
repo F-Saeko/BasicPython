7d9e6297801fd174c3f11ee69178f91e47fe8548
F-Saeko <s.fuwasaki.24.8.1566@4nonome.com>
2024-09-05 20:30:50 +0900

    問3完了

--- euclid.py
+++ euclid.py
@@ -1,4 +1,32 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+#(1)
+a1 = 10
+b1 = 20
 
-# TODO
+while b1 != 0:
+    c1 = a1 % b1
+    a1 = b1
+    b1 = c1
+
+print(a1)
+
+#(2)
+a2 = 14
+b2 = 91
+
+while b2 != 0:
+    c2 = a2 % b2
+    a2 = b2
+    b2 = c2
+
+print(a2)
+
+#(3)
+a3 = 91
+b3 = 14
+
+while b3 != 0:
+    c3 = a3 % b3
+    a3 = b3
+    b3 = c3
+
+print(a3)
