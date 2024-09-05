23895027037a7e065657246e53412a911997b8c8
F-Saeko <s.fuwasaki.24.8.1566@4nonome.com>
2024-09-05 19:49:11 +0900

    問2完了

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,11 @@
+import math
 from math import sin
-# --example--
-# print(sin(0))
-# >>> 0
-# -----------
+
+h = (math.pi/2)/100
+integral = 0.5 * (sin(0)+sin(math.pi/2))
+
+for i in range(1 ,100):
+    integral += sin(i*h)
+integral *= h
+
+print(integral)
