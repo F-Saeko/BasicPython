c3b428c1bc69ff0854e5a3a2506a1921ef9b5809
F-Saeko <s.fuwasaki.24.8.1566@4nonome.com>
2024-09-08 14:43:41 +0900

    関数_問1完了

--- prime_number.py
+++ prime_number.py
@@ -1,22 +1,29 @@
-a = 61
-b = 10
+def prime_number(n):
+    if n <= 0:
+        raise ValueError("エラー: 自然数ではありません")
+    elif n == 1:
+        raise ValueError("エラー: 1より大きい自然数を入力してください")
 
-if a <= 1:
-    print(str(a) + "は素数ではありません")
-else:
-    for i in range(2, int(a**0.5)+1):
-        if a % i == 0:
-            print(str(a) + "は素数ではありません")
-            break
     else:
-        print(str(a) + "は素数です")
+        for i in range(2, int(n**0.5)+1):
+            if n % i == 0:
+                return False
+        return True
+   
+while True:
+    try:
+        n = float(input("1より大きい自然数を入力してください: "))
+        if not n.is_integer():
+            raise ValueError("エラー: 自然数ではありません")
+    
+        n = int(n)
+        result = prime_number(n)
+        if result:
+            print(f"{n}は素数です")
+        else:
+            print(f"{n}は素数ではありません")
+        break
 
-if b <= 1:
-    print(str(b) + "は素数ではありません")
-else:
-    for i in range(2, int(b**0.5)+1):
-        if b % i == 0:
-            print(str(b) + "は素数ではありません")
-            break
-    else:
-        print(str(b) + "は素数です")
+    except (ValueError, TypeError) as e:
+        print(e)
+        print("もう一度入力してください")
