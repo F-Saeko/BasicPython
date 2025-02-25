160c12d9703d7375e904ef1fa6d6d047d433522e
F-Saeko <s.fuwasaki.24.8.1566@4nonome.com>
2025-02-25 17:00:59 +0900

    関数_問4完了

--- euclid.py
+++ euclid.py
@@ -5,14 +5,32 @@ def euclid(a, b):
         b = c
     return a
 
-#(1)
+#(3-1)
 result1 = euclid(10, 20)
 print(result1)
 
-#(2)
+#(3-2)
 result2 = euclid(14, 91)
 print(result2)
 
-#(3)
+#(3-3)
 result3 = euclid(91, 14)
 print(result3)
+
+
+#問4
+def Coprime(a,b):
+   result = euclid(a,b)
+   if result == 1:
+       print("互いに素")
+   else:
+       print("互いに素ではない")
+
+#(4-1)
+Coprime(10,20)
+
+#(4-2)
+Coprime(14,91)
+
+#(4-3)
+Coprime(91,14)
